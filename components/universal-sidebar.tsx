e3333944a61e9c77d3f57a9a6bd77d5bf951f65f
EratosVoid <aneeshbhatcs009@gmail.com>
2025-06-14 20:23:31 +0530

    UI Improvements

--- components/universal-sidebar.tsx
+++ components/universal-sidebar.tsx
@@ -98,7 +98,7 @@ export function UniversalSidebar({ isOpen, onToggle }: UniversalSidebarProps) {
       {/* Sidebar */}
       <div
         className={`
-          fixed inset-y-0 left-0 z-50 w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out flex flex-col
+          fixed inset-y-0 left-0 z-50 w-64 bg-white dark:bg-gray-900 shadow-lg transform transition-transform duration-300 ease-in-out flex flex-col
           ${isOpen ? "translate-x-0" : "-translate-x-full"}
           lg:translate-x-0 lg:static lg:inset-0
         `}
